3243a929fd92721c2641bef7d35052b4bd68b8b7
Suraj Gupta <65316879+abracoder@users.noreply.github.com>
2023-09-10 18:12:44 +0530

    Time: 6 ms (88.91%), Space: 7.8 MB (86.32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
-    
+    int sol(string s){
+        int n = s.length();
+        int maxLength = 0;
+        vector<int> charIndex(128, -1);
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (charIndex[s[right]] >= left) {
+                left = charIndex[s[right]] + 1;
+            }
+            charIndex[s[right]] = right;
+            maxLength = max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
     
     int lengthOfLongestSubstring(string s) {
+        return sol(s);
         int n = s.length();
         int maxLength = 0;
         unordered_map<char, int> charMap;
