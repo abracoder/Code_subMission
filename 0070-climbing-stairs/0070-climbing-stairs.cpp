3fa203a0b4fc0c51be84897ae20385e311074307
Suraj Gupta <65316879+abracoder@users.noreply.github.com>
2023-09-19 17:31:02 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -18,8 +18,15 @@ public:
     
     int climbStairs(int n) {
         int steps = 0;
-        vector < int> dp(n+1,-1);
-       return noOfWays(0,n,dp);
+        vector < int> dp(n+1, 0);
+        dp[n-1] = 1 ;
+        dp[n] = 1;
+        for (int i = n-2; i >= 0 ; i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        
+        
+       return dp[0];
         
         
     }
