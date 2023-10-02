ade95b23ba3869f395a1f46e8695d3d164c6aa63
Suraj Gupta <65316879+abracoder@users.noreply.github.com>
2023-10-02 19:11:49 +0530

    Time: 4 ms (23.08%), Space: 8.1 MB (20.08%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int solve(int ind, vector<int> &nums,vector<int> &dp){
+        
+        if(ind == 0) return nums[ind];
+        if(ind <0) return 0;
+        
+        if(dp[ind]!=-1) return dp[ind];
+        
+        
+        int pick = nums[ind] + solve(ind-2,nums,dp);
+        
+        int notPick = 0 + solve(ind-1,nums,dp);
+        
+        
+        return dp[ind] = max(pick,notPick);
+        
+        
+        
+    }
+    
+    
+public:
+    int rob(vector<int>& nums) {
+        
+        vector<int> dp(nums.size(),-1);
+        
+        return solve(nums.size()-1, nums,dp);
+        
+        
+    }
+};
