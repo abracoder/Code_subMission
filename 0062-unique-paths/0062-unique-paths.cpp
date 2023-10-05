58f74fe8c280ed460844525798da1d0b9c98cb76
Suraj Gupta <65316879+abracoder@users.noreply.github.com>
2023-10-05 11:56:05 +0545

    Time: 0 ms (100.00%), Space: 7 MB (5.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,7 +12,19 @@ class Solution {
     
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(0,0, m,n,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[m-1][n-1] = 1;
+        for(int i = m-1;i>=0;i--){
+            for(int j = n-1; j >=0; j--){
+                dp[i][j] = max(dp[i][j],dp[i][j+1] + dp[i+1][j]);
+                
+            }
+        }
+        
+        return dp[0][0];
+        
+        
+        
+        // return solve(0,0, m,n,dp);
     }
 };
