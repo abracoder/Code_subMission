080599305319a2ef6580d0fed941503536458a00
Suraj Gupta <65316879+abracoder@users.noreply.github.com>
2023-10-18 03:44:37 +0545

    Time: 35 ms (8.14%), Space: 16.7 MB (10.05%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,13 +1,12 @@
 class Solution {
 void solve(int ind,int target,vector<int>& arr,vector<vector<int>> &ans,vector<int> ds){
-
+  if(target < 0) return;
   if (target == 0) {
     ans.push_back(ds);
     return;
   }
   for (int i = ind; i < arr.size(); i++) {
     if (i > ind && arr[i] == arr[i - 1]) continue;
-    if (arr[i] > target) break;
     ds.push_back(arr[i]);
     solve(i + 1, target - arr[i], arr, ans, ds);
     ds.pop_back();
