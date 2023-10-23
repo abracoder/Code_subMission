770f2f2d9d4109e3722f716654bc9a032daa2970
Suraj Gupta <65316879+abracoder@users.noreply.github.com>
2023-10-23 10:22:07 +0545

    Time: 1842 ms (5.00%), Space: 489.6 MB (5.13%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    
+//     void longestUtil(string s, int index, string &ans){
+        
+//         for(int i =0; i< s.size();i++){
+//             for(int j =s.size()-1; j>i; j--){
+            
+//             if(isPallindrome(s,i,j)){
+//                 string temp=(s.substr(i,j));
+//                 ans = temp;
+//                 return;
+                
+//             }
+//             }
+//         }
+//         ans = "";
+//         return;
+//     }
+    
+    bool isPallindrome(string &s, int start ,int end){
+        while( start < end){
+            if(s[start++] != s[end--])
+                return false;
+        }
+        return true;
+    }
+    
+public:
+    string longestPalindrome(string s) {
+        string ans;
+        if(s.size() <2) return s;
+        for(int i = 0; i<s.size(); i++){
+            for(int j= i; j < s.size(); j++){
+                if(isPallindrome(s,i,j)){
+                    string t = s.substr(i,j-i+1);
+                    ans = ans.size()>t.size()?ans: t;
+                }
+            }
+        }
+    
+      return ans;
+    }
+};
